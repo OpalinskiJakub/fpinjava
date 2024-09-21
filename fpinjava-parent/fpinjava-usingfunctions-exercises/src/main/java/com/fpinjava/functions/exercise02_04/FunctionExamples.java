47cd0bc6d1e39508f24de17d4fffe241b69b6edb
OpalinskiJakub <jakopal1125@gmail.com>
2024-09-21 19:03:41 +0200

    task 02_04

--- fpinjava-parent/fpinjava-usingfunctions-exercises/src/main/java/com/fpinjava/functions/exercise02_04/FunctionExamples.java
+++ fpinjava-parent/fpinjava-usingfunctions-exercises/src/main/java/com/fpinjava/functions/exercise02_04/FunctionExamples.java
@@ -7,7 +7,36 @@ public class FunctionExamples {
   public static final Function<Integer, Integer> square = x -> x * x;
 
   public static final Function<Function<Integer, Integer>, Function<Function<Integer, Integer>,
-                                        Function<Integer, Integer>>> compose = null; // To be implemented
+                                        Function<Integer, Integer>>> compose =
+          new Function<Function<Integer, Integer>, Function<Function<Integer, Integer>, Function<Integer, Integer>>>(){
+            @Override
+            public Function<Function<Integer, Integer>, Function<Integer, Integer>> apply(Function<Integer, Integer> f1) {
+              return new Function<Function<Integer, Integer>, Function<Integer, Integer>>() {
+                @Override
+                public Function<Integer, Integer> apply(Function<Integer, Integer> f2) {
+                  return new Function<Integer, Integer>() {
+                    @Override
+                    public Integer apply(Integer arg) {
+                      return f1.apply(f2.apply(arg));
+                    }
+                  };
+                }
+              };
+            }
+          };
 
-  public static final Function<Integer, Integer> f = compose.apply(square).apply(triple);
+  public static final Function<Function<Integer, Integer>, Function<Function<Integer, Integer>,
+          Function<Integer, Integer>>> compose2 =
+          f1 -> f2 -> x -> f1.apply(f2.apply(x));
+
+
+  public static final Function<Integer, Integer> f = compose2.apply(square).apply(triple);
+
+  public static final Function<Integer, Integer> f1 = compose.apply(square).apply(triple);
+
+  public static void main(String[] args) {
+    System.out.println(f.apply(1));
+    System.out.println(f.apply(1));
+
+  }
 }
